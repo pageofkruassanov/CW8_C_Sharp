7591c515e62a20282b6b6aacd0173087ab4a809c
Pavlo <pkhasanov@gmail.com>
2023-10-05 20:55:21 +0300

    Finish Car class

--- CW8_C_Sharp/Properties/Car.cs
+++ CW8_C_Sharp/Properties/Car.cs
@@ -6,8 +6,48 @@ using System.Threading.Tasks;
 
 namespace CW8_C_Sharp.Properties
 {
-    //internal class Car : IVehicle, IDriveable
-    //{
-        
-    //}
+    internal class Car : IVehicle, IDriveable
+    {
+        private string _model;
+
+        private int _side;
+
+        public int GetSide()
+        { 
+            return _side; 
+        }
+
+
+        public Car(string model)
+        {
+            _model = model;
+        }
+        public override int Speed { get; set; }
+
+        public override double FuelLevel { get; set; }
+
+        public override void Start()
+        {
+            Console.WriteLine($"{_model} начинает движение");
+        }
+
+        public override void Stop()
+        {
+            Console.WriteLine($"{_model} заканчивает движение");
+        }
+
+        public void Accelerate(int speed)
+        {
+            Speed += speed;
+        }
+        public void Brake(int speed)
+        {
+            Speed -= speed;
+        }
+        public void Turn(int side)
+        {
+            _side = side;
+        }
+
+    }
 }
